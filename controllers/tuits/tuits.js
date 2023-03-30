168455512d6e0273df80df399998902d6a9574e5
rehmaund <rehmaund44@gmail.com>
2023-03-30 15:38:42 -0400

    a8 section 6

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -4,7 +4,7 @@ export default [
         "handle": "@spacex", "time": "2h", "image": "/images/spacex.jpg",
         "title": "SpaceX's Mission",
         "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
-        "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+        "liked": false, "likes": 2345, "replies": 123, "retuits": 432
     },
     {
         "_id": "234", "topic": "Traffic", "username": "The Boring Company",
@@ -18,6 +18,6 @@ export default [
         "handle": "@neurolink", "time": "3h", "image": "/images/grimes.jpg",
         "title": "Neurolink fixes paralisis and blindness",
         "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
-        "liked": true, "likes": 3456, "replies": 345, "retuits": 654
+        "liked": false, "likes": 3456, "replies": 345, "retuits": 654
     },
 ];
